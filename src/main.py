a55ceca6f8f22bebd448e98c861aad667b05099c
XeybienB <166884360+XeybienB@users.noreply.github.com>
2026-02-27 15:19:33 -0500

    Update main.py
    Organized it to fix Simulation class

--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
 from Simulation import Simulation
-from inventory import InventoryBin
-from metrics import Metrics
+
 
 if __name__ == "__main__":
+    print("=================================")
     print("Starting Warehouse Simulation...")
+    print("=================================\n")
 
     sim = Simulation(arrival_rate=0.5)
     sim.run(num_orders=5)
-
-    inventory = InventoryBin(stock=20, reorder_point=5, reorder_qty=15)
-    inventory.remove_stock(10)
-    inventory.remove_stock(8)
-
-    metrics = Metrics()
-    metrics.record_order()
-    metrics.summary()# Warehouse Simulation Project
-# Entry point for the simulation
